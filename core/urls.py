5b6cc1642ed2dda0de4505baf66ad130aac60f11
Letícia Fernandes <lemayara16@gmail.com>
2020-09-18 19:29:53 -0300

    criação arquivos para views

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from core.views.index import index
 from core.views.pessoa import cadastro_pessoa, listagem_pessoa
 from core.views.funcionario import cadastro_funcionario, listagem_funcionario
 from core.views.veterinario import cadastro_veterinario, listagem_veterinario
+from core.views.adocao import cadastro_adocao, listagem_adocao
 
 
 urlpatterns = [
@@ -18,5 +19,9 @@ urlpatterns = [
     # Veterinário
     path('cadastro_veterinario/', cadastro_veterinario, name='url_cadastro_veterinario'),
     path('listagem_veterinario/', listagem_veterinario, name='url_listagem_veterinario'),
+
+    # Adoção
+    path('cadastro_adocao/', cadastro_adocao, name='url_cadastro_adocao'),
+    path('listagem_adocao/', listagem_adocao, name='url_listagem_adocao'),
 
 ]
