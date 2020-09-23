ca44573eea292981e653f12c4bea19ccc7738a0c
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-09-23 15:45:55 -0300

    Pesquisa na listagem de guardiões

--- core/views/pessoa.py
+++ core/views/pessoa.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.db import transaction
 from core.forms import FormPessoa
 from core.models import Pessoa, Funcionario, Veterinario
+from django.db.models import Q
 
 url_listagem = '/listagem_pessoa'
 url_cadastro = '/cadastro_pessoa'
@@ -30,13 +31,17 @@ def cadastro_pessoa(request):
 
 @login_required()
 def listagem_pessoa(request):
-    pessoas = Pessoa.objects.all()
+    search = request.GET.get('search', '')
+
+    pessoas = Pessoa.objects.filter(Q(nome__icontains=search) |
+                                    Q(cpf__icontains=search))
 
     context = {'dados': pessoas,
                'title_page': 'Listagem',
                'btn_action': 'Registrar',
                'model': model,
-               'url': url_cadastro
+               'url': url_cadastro,
+               'search': search
                }
 
     return render(request, 'core/pessoa/listagem_pessoa.html', context)
